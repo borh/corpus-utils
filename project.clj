03e1f83cf555209b0d3bcaed1768f1912f381844
Bor Hodošček <bor.hodoscek@gmail.com>
2014-01-30 22:28:38 +0900

    new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject corpus-utils "0.0.5.1"
+(defproject corpus-utils "0.0.6"
   :description "Miscellaneous utilities to parse Japanese language corpora with Clojure"
   :url "https://github.com/borh/corpus-utils.git"
   :license {:name "Eclipse Public License"
