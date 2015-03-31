9e2e90d92d608cfd6458652c43aa4401da0a6e8d
Bor Hodošček <bor.hodoscek@gmail.com>
2015-03-31 12:55:12 +0900

    updated deps and new release

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject corpus-utils "0.1.4"
+(defproject corpus-utils "0.1.5"
   :description "Miscellaneous utilities to parse Japanese language corpora with Clojure"
   :url "https://github.com/borh/corpus-utils.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
 
-                 [clj-mecab "0.4.1.1"]
+                 [clj-mecab "0.4.1.2"]
 
-                 [fast-zip "0.6.0" :exclusions [com.cemerick/austin]]
+                 [fast-zip "0.6.1" :exclusions [com.cemerick/austin]]
                  [org.clojure/data.csv "0.1.2"]
                  [me.raynes/fs "1.4.6"]
                  [org.apache.commons/commons-compress "1.9"]
                  [org.tukaani/xz "1.5"]
 
                  [prismatic/schema "0.4.0"]
-                 [prismatic/plumbing "0.4.0"]]
+                 [prismatic/plumbing "0.4.1"]]
   :min-lein-version "2.0.0"
   :resource-paths ["data"]
   :jvm-opts ["-server"]
