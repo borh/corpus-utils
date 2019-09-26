cf4c44d67fda5e4fa2c496f8758bf3208112cb70
Bor Hodošček <dev@bor.space>
2019-09-26 17:13:11 +0900

    migrated to spec

--- src/corpus_utils/document.clj
+++ src/corpus_utils/document.clj
@@ -1,69 +1,66 @@
 (ns corpus-utils.document
-  (:require [schema.core :as s]))
+  (:require [clojure.spec.alpha :as s]))
 
-(def opt s/optional-key)
+;; Required minimal metadata
+(s/def :metadata/title string?)
+(s/def :metadata/author string?)
+(s/def :metadata/year int?)
+(s/def :metadata/basename string?)
+(s/def :metadata/category (s/coll-of string?))
+(s/def :metadata/permission boolean?)
 
-(def BCCWJAnnotationSchema
-  {(opt :forward-or-afterward) Boolean
-   (opt :dialog)               Boolean
-   (opt :quotation-type)       Boolean
-   (opt :visual)               Boolean
-   (opt :db-or-list)           Boolean
-   (opt :archaic)              Boolean
-   (opt :foreign-language)     Boolean
-   (opt :math-or-code)         Boolean
-   (opt :legalese)             Boolean
-   (opt :questionable-content) Boolean
-   (opt :low-content)          Boolean
-   (opt :target-audience)      (s/enum 1 2 3 4 5)
-   (opt :hard-soft)            (s/enum 1 2 3 4)
-   (opt :informality)          (s/enum 1 2 3)
-   (opt :addressing)           (s/enum 1 2 3)
-   (opt :protagonist-personal-pronoun) s/Str
-   (opt :other-addressing)     (s/enum 1 2 3 4)})
+;; BCCWJ metadata
+(s/def :metadata/gender #{:male :female :mixed})
+(s/def :metadata/author-year int?)
+(s/def :metadata/publisher string?)
+(s/def :metadata/copyright string?)
+(s/def :metadata/audience string?)
+(s/def :metadata/media string?)
+(s/def :metadata/topic string?)
+(s/def :metadata/corpus string?)
+(s/def :metadata/subcorpus string?)
+(s/def :metadata/script string?)
 
-(def MetadataSchema
-  "Schema of a document's possible metadata"
-  (merge BCCWJAnnotationSchema
-         {:title     s/Str
-          :author    s/Str
-          (opt :gender) (s/enum :male :female :mixed)
-          (opt :author-year) s/Int
-          :publisher s/Str
-          (opt :copyright) s/Str
-          (opt :audience) s/Str
-          (opt :media) s/Str
-          (opt :topic) s/Str
-          :year      s/Int
-          :basename  s/Str
-          :corpus    s/Str
-          (opt :script) s/Str
-          (opt :subcorpus) s/Str
-          :category  [s/Str]}))
+;; BCCWJ extra annotations
+(s/def :metadata/forward-or-afterward boolean?)
+(s/def :metadata/dialog boolean?)
+(s/def :metadata/quotation-type boolean?)
+(s/def :metadata/visual boolean?)
+(s/def :metadata/db-or-list boolean?)
+(s/def :metadata/archaic boolean?)
+(s/def :metadata/foreign-language boolean?)
+(s/def :metadata/math-or-code boolean?)
+(s/def :metadata/legalese boolean?)
+(s/def :metadata/questionable-content boolean?)
+(s/def :metadata/low-content boolean?)
+(s/def :metadata/target-audience #{1 2 3 4 5})
+(s/def :metadata/hard-soft #{1 2 3 4})
+(s/def :metadata/informality #{1 2 3})
+(s/def :metadata/addressing #{1 2 3})
+(s/def :metadata/protagonist-personal-pronoun string?)
+(s/def :metadata/other-addressing #{1 2 3 4})
 
-(def SentencesSchema
-  [{:tags clojure.lang.PersistentHashSet
-    :sentences [s/Str]}])
+(s/def :document/metadata
+  (s/keys :req
+          [:metadata/title :metadata/author :metadata/year :metadata/basename
+           :metadata/category :metadata/permission]
+          :opt
+          [:metadata/gender :metadata/author-year :metadata/publisher :metadata/copyright
+           :metadata/audience :metadata/media :metadata/topic
+           :metadata/corpus :metadata/subcorpus :metadata/script
 
-(def DocumentSchema
-  {:paragraphs SentencesSchema
-   :metadata   MetadataSchema})
+           :metadata/forward-or-afterward :metadata/dialog :metadata/quotation-type :metadata/visual
+           :metadata/db-or-list :metadata/archaic :metadata/foreign-language :metadata/math-or-code
+           :metadata/legalese :metadata/questionable-content :metadata/low-content :metadata/target-audience
+           :metadata/hard-soft :metadata/informality :metadata/addressing :metadata/protagonist-personal-pronoun
+           :metadata/other-addressing]))
 
-(def UnidicMorphemeSchema
-  {:pos-1 s/Str
-   :pos-2 s/Str
-   :pos-3 s/Str
-   :pos-4 s/Str
-   :c-type s/Str
-   :c-form s/Str
-   :l-form s/Str
-   :lemma s/Str
-   :orth s/Str
-   :pron s/Str
-   :orth-base s/Str
-   :pron-base s/Str
-   :goshu s/Str
-   :i-type s/Str
-   :i-form s/Str
-   :f-type s/Str
-   :f-form s/Str})
+(s/def :sentence/text string?)
+(s/def :paragraph/sentences (s/coll-of :sentence/text))
+(s/def :paragraph/tags (s/coll-of keyword?))
+(s/def :document/paragraph (s/keys :req [:paragraph/tags :paragraph/sentences]))
+(s/def :document/paragraphs (s/coll-of :document/paragraph))
+(s/def :corpus/document (s/keys :req [:document/paragraphs]
+                                ;; FIXME Optional :basename at top level for documents with external metadata.
+                                :opt [:document/metadata :document/basename]))
+(s/def :corpus/documents (s/coll-of :corpus/document))
